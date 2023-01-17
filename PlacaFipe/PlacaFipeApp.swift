972fdbbfe34dc2ef0364e0fe50f1d03f1855a561
Ricieri Faviero <98041808+ricifaviero@users.noreply.github.com>
2023-01-17 08:48:44 -0300

    Initial Commit

---
+++ PlacaFipe/PlacaFipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacaFipeApp.swift
+//  PlacaFipe
+//
+//  Created by Ricieri Faviero on 17/01/23.
+//
+
+import SwiftUI
+
+@main
+struct PlacaFipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
